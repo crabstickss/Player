03e60f9798b76bdf35d4fbf7f7e7ac339f187a28
maaar18 <marovs18@mail.ru>
2022-06-03 10:57:16 +0300

    Move musicFolderCheck() and currentListCheck to the Utils class

--- src/main/java/com/javafx/mediaplayer/Utils.java
+++ src/main/java/com/javafx/mediaplayer/Utils.java
@@ -2,6 +2,10 @@ package com.javafx.mediaplayer;
 
 import javafx.util.Duration;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Utils {
     protected static String formatTime(Duration elapsed, Duration duration) {
         int intElapsed = (int)Math.floor(elapsed.toSeconds());
@@ -18,6 +22,30 @@ public class Utils {
         } else {
             return String.format("%02d:%02d",elapsedMinutes,
                     elapsedSeconds);
+        }
+    }
+    protected static void musicDataFolderCheck() {
+        File musicData = new File("musicData");
+        if (!musicData.exists()) {
+            musicData.mkdir();
+        }
+    }
+    protected static void currentListCheck() {
+        File file = new File("musicData/currentList.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try {
+                FileWriter fw = new FileWriter(file.toString());
+                fw.write("");
+                fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
