45d9a86adc1e30124e43c58495ebdff7a7861384
Ivan44k <b0gachevv@bk.ru>
2022-06-06 02:08:28 +0300

    Addition of playlists in playlistController

--- src/main/java/com/javafx/mediaplayer/playlistController.java
+++ src/main/java/com/javafx/mediaplayer/playlistController.java
@@ -30,11 +30,39 @@ public class playlistController implements Initializable {
         playlistsList = new ArrayList<>();
         Utils.refreshPlaylistsList(playlistsList);
         initPlaylistsListView();
+        onSelectionPlaylistsView();
     }
     public void enterPlaylistName() {
     }
 
     public void addPlaylistOk() {
+        addPlaylistOkButton.setOnMouseClicked(mouseEvent -> {
+            String songToAdd = FXMLController.curSongs.get(FXMLController.songNumber).toString();
+            if (playlistTextField.getText().equals("") && !isPlaylistSelected) {
+                createDefaultPlaylist(songToAdd);
+            } else if (isPlaylistSelected) {
+                Utils.createPlaylist(addPlaylistListView.getSelectionModel().getSelectedItem(), songToAdd);
+            } else {
+                if (!playlistTextField.getText().equals("")) {
+                    Utils.createPlaylist(playlistTextField.getText(), songToAdd);
+                }
+                else {
+                    createDefaultPlaylist(songToAdd);
+                }
+            }
+            closePlaylists();
+        });
+    }
+
+    private void createDefaultPlaylist(String song) {
+        File file = new File("musicData/playlists.txt");
+        int number;
+        try {
+            number = Files.readString(file.toPath()).split("\n").length;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Utils.createPlaylist("Playlist #" + number, song);
     }
 
     public void addPlaylistCancel() {
@@ -45,6 +73,25 @@ public class playlistController implements Initializable {
     private void initPlaylistsListView() {
         ObservableList<String> observableList = FXCollections.observableList(playlistsList);
         addPlaylistListView.setItems(observableList);
+    }
+    private void onSelectionPlaylistsView() {
+        addPlaylistListView.getSelectionModel().selectedItemProperty().addListener((observableValue, s, t1) -> {
+            addPlaylistListView.setOnMouseClicked(mouseEvent -> {
+                isPlaylistSelected = true;
+                if (mouseEvent.getClickCount() == 2) {
+                    String selectedPlaylist = addPlaylistListView.getSelectionModel().getSelectedItem();
+                    String songToAdd = FXMLController.curSongs.get(FXMLController.songNumber).toString();
+                    String playlistPath = "musicData/playlists/" + selectedPlaylist + ".txt";
+                    File playlistsFile = new File(playlistPath);
+                    try {
+                        Files.write(playlistsFile.toPath(), songToAdd.getBytes(), StandardOpenOption.APPEND);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    closePlaylists();
+                }
+            });
+        });
     }
     private void closePlaylists() {
         Stage stage = (Stage) addPlaylistCancelButton.getScene().getWindow();
