9e07271230cb7205535c202ee936293d872f2a83
kwsjd <slavauvanov2002@gmail.com>
2022-06-05 19:40:12 +0300

    - Add controller for playlist windows

---
+++ src/main/java/com/javafx/mediaplayer/playlistController.java
@@ -0,0 +1,22 @@
+package com.javafx.mediaplayer;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class playlistController {
+    public Button addPlaylistOkButton;
+    public TextField playlistTextField;
+    public Button addPlaylistCancelButton;
+    public ListView addPlaylistListView;
+
+    public void enterPlaylistName(ActionEvent actionEvent) {
+    }
+
+    public void addPlaylistOk(ActionEvent actionEvent) {
+    }
+
+    public void addPlaylistCancel(ActionEvent actionEvent) {
+    }
+}
