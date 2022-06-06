43885d5c32cbb4f907064f7c3bb17e3d31e1972c
lakuzn <91047299+lakuzn@users.noreply.github.com>
2022-06-07 02:34:47 +0300

    first tests file

---
+++ src/main/java/com/javafx/mediaplayer/UtilsTest.java
@@ -0,0 +1,38 @@
+package com.javafx.tests;
+
+import com.javafx.mediaplayer.Utils;
+import javafx.util.Duration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UtilsTest {
+    public UtilsTest() {
+    }
+
+    @Test
+    public void formatTime() {
+        Duration duration = new Duration(20000.0D);
+        Duration elapsed = new Duration(5000.0D);
+        String expected = "00:05/00:20";
+        String actual = Utils.formatTime(elapsed, duration);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void formatTimeDurationZero() {
+        Duration duration = new Duration(0.0D);
+        Duration elapsed = new Duration(2000.0D);
+        String expected = "00:02";
+        String actual = Utils.formatTime(elapsed, duration);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void currentListCheck() {
+    }
+
+
+}
